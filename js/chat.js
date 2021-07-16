286b6039ed229d71a968526d368203eb7fd1c169
yarikfanat <85844920+yarikfanat@users.noreply.github.com>
2021-07-16 20:18:28 +0300

    Add files via upload

--- js/chat.js
+++ js/chat.js
@@ -628,6 +628,8 @@ function hangUpCall() {/*
   mediaConstraints.video.aspectRatio.ideal = 1.0;
   console.log ('меняю aspectRatio на',mediaConstraints.video.aspectRatio.ideal);
   console.log ('высота области=',container.clientHeight,'px ширина области=',container.clientWidth,'px');
+  var txt='высота области='+container.clientHeight+'px ширина области='+container.clientWidth+'px';
+  alert (txt);
    try {
    	console.log ('video tracks=',webcamStream.getVideoTracks().length);
       webcamStream.getVideoTracks().forEach(
