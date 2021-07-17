d61e3a0bf5e846769881e05bf5cb482e5540f3b4
yarikfanat <85844920+yarikfanat@users.noreply.github.com>
2021-07-17 17:57:01 +0300

    Add files via upload

--- js/chat.js
+++ js/chat.js
@@ -650,12 +650,14 @@ function hangUpCall() {
 		headwrap.style.height='120px';
 		head_camera.style.display = 'block';
 	  	head_chat.style.display = 'block';
-	  	container.style.marginTop = '20px';
+	 	container.style.width = '100%';
 	  	container.style.height = '380px';
 	  	container.style.zIndex = '2';
 		container.style.gridTemplateAreas = '"head-chat head-camera" "camerabox camerabox"'
 		container.style.gridTemplateRows = 'auto 1fr';
 		container.style.gridTemplateColumns = '60% 1fr';
+		video_chat.style.width = '100%';
+	  	video_chat.style.height = '100%';
 
 		try  {
 			received_video.srcObject.getVideoTracks().forEach((track)=> {
